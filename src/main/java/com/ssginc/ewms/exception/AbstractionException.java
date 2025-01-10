bd4b2b5f80f7582590a865494455cba350fccbe8
Kim Daecheol <kdc9619@naver.com>
2025-01-10 18:23:34 +0900

    fix. 예외처리 최상위 추상클래스 수정
    - 멤버변수로 status 추가
    - toString() 메소드 삭제
    - 생성자에 log.error() 출력하도록 구현

--- src/main/java/com/ssginc/ewms/exception/AbstractionException.java
+++ src/main/java/com/ssginc/ewms/exception/AbstractionException.java
@@ -2,6 +2,7 @@ package com.ssginc.ewms.exception;
 
 import com.ssginc.ewms.util.ErrorCode;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 애플리케이션의 공통 예외 처리 추상 클래스.
@@ -10,11 +11,13 @@ import lombok.Getter;
  * 예외 처리 시 일관된 에러 코드 및 메시지를 사용할 수 있도록 설계되었습니다.
  */
 @Getter
+@Slf4j
 public abstract class AbstractionException extends Exception {
 
     /**
      * 에러 코드 번호
      */
+    private final int status;
     private final String code;
 
     /**
@@ -55,11 +58,8 @@ public abstract class AbstractionException extends Exception {
      */
     protected AbstractionException(ErrorCode errorCode, Object[] args, Throwable cause) {
         super(args != null ? String.format(errorCode.getMsg(), args) : errorCode.getMsg(), cause);
+        this.status = errorCode.getStatus();
         this.code = errorCode.getCode();
-    }
-
-    @Override
-    public String toString() {
-        return "Error Code: " + code + ", Message: " + getMessage();
+        log.error(errorCode.getStatus() + " " + errorCode.getCode() + " " + errorCode.getMsg(), cause);
     }
 }
